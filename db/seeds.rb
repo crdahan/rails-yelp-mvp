274daa0a27052b6cbd0ac0082560272b6c6121d3
Clara Dahan <crdahan@gmail.com>
2023-05-24 21:15:57 +0100

    railsyelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+dishoom = { name: "Dishoom", phone_number: "123456778", address: "Shoreditch", category: "french" }
+sanssouci = { name: "Sans Souci", phone_number: "3323456778", address: "Pigalle", category: "french" }
+
+Restaurant.create(dishoom)
+Restaurant.create(sanssouci)
